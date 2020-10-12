415dbe866845b54379ce285c2c336dd33f581dab
Ilya Shved <ilia83ivanovich@gmail.com>
2020-10-12 14:39:52 +0500

    lesson_21-versDialog

--- src/components/dialogs/Dialog.js
+++ src/components/dialogs/Dialog.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import classes from './Dialog.module.css';
+import cl from './Dialog.module.css';
 
 class Dialog extends React.Component {
   render() {
     return (
-      <div >
-        Dialog
-        Здесь  унас будут диалоги общающихся
+      <div className={cl.dialogs}>
+        <div className={cl.dialogItem}>
+          <div className={cl.dialog +' '+cl.active}>
+            Николай
+          </div>
+          <div className={cl.dialog}>
+            Наталья
+          </div>
+          <div className={cl.dialog}>
+            Василий
+          </div>
+          <div className={cl.dialog}>
+            Екатерина
+          </div>
+          <div className={cl.dialog}>
+            Петр
+          </div>
+          <div className={cl.dialog}>
+            Светлана
+          </div>
+          <div className={cl.dialog}>
+            Сергей
+          </div>
+
+        </div>
+        <div className={cl.messages}>
+          <div className={cl.message}>Че каво?</div>
+          <div className={cl.message}>привет , ну как то ттак, ничего очобенного</div>
+          <div className={cl.message}>Ну давай . мног не давай</div>
+
+        </div>
+
       </div>
     );
   }
