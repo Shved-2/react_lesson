24bb3dace2b2797447c2406b5e3b8fe67088ef5b
Ilya Shved <ilia83ivanovich@gmail.com>
2020-10-12 10:52:17 +0500

    lesson_20-navlink

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import cl from'./Navbar.module.css';
+import cl from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 class NavBar extends React.Component {
   render() {
     return (
 
       <nav className={cl.nav}>
-        <div className={cl.item}><a href="/profiles">Profile</a></div>
-        <div className={`${cl.item} ${cl.activ}`}><a href="/dialogs">Message</a></div>
-        <div className={cl.item}><a href="/news">News</a></div>
-        <div className={cl.item}><a href="/music">Music</a></div>
-        <div className={cl.item}><a href="/setting">Setting</a></div>
-        
+        <div className={cl.item}>
+          <NavLink to="/profiles" activeClassName={cl.active}>Profile</NavLink>
+        </div>
+        <div className={cl.item}>
+          <NavLink to="/dialogs" activeClassName={cl.active}>Message</NavLink></div>
+        <div className={cl.item}>
+          <NavLink to="/news" activeClassName={cl.active}>News</NavLink>
+        </div>
+        <div className={cl.item}>
+          <NavLink to="/music" activeClassName={cl.active}>Music</NavLink>
+        </div>
+        <div className={cl.item}>
+          <NavLink to="/setting" activeClassName={cl.active}>Setting</NavLink>
+        </div>
+
       </nav>
 
     );
