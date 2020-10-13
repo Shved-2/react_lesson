c5d3650c28a57505a00f164626617165c6c183b4
Ilya Shved <ilia83ivanovich@gmail.com>
2020-10-14 00:51:25 +0500

    lesson_25-map

--- src/components/dialogs/Dialog.js
+++ src/components/dialogs/Dialog.js
@@ -37,31 +37,26 @@ class Dialog extends React.Component {
       { message: "йоу", id: 4 },
       { message: "йоу", id: 5 }
     ]
+
+    let DialogElement = dialogData.map((d) => {
+      return (
+        <DialogItem name={d.name} id={d.id} />
+      )
+    })
+
+    let MessageElement = messagesData.map((m) => {
+      return (
+        <Message message={m.message} />
+      )
+    })
     return (
       <div className={cl.dialogs}>
         <div className={cl.dialogItem}>
-
-          <DialogItem name={dialogData[0].name} id={dialogData[0].id} />
-          <DialogItem name={dialogData[1].name} id={dialogData[1].id} />
-          <DialogItem name={dialogData[2].name} id={dialogData[2].id} />
-          <DialogItem name={dialogData[3].name} id={dialogData[3].id} />
-          <DialogItem name={dialogData[4].name} id={dialogData[4].id} />
-          <DialogItem name={dialogData[5].name} id={dialogData[5].id} />
-          <DialogItem name={dialogData[6].name} id={dialogData[6].id} />
-          <DialogItem name={dialogData[7].name} id={dialogData[7].id} />
-
-
-
-
-
+          {DialogElement}
         </div>
+        
         <div className={cl.messages}>
-          <Message message={messagesData[0].message} />
-          <Message message={messagesData[1].message} />
-          <Message message={messagesData[2].message} />
-          <Message message={messagesData[3].message} />
-          <Message message={messagesData[4].message} />
-
+          {MessageElement}
         </div>
 
       </div>
--- src/components/profiles/myPosts/MyPost.js
+++ src/components/profiles/myPosts/MyPost.js
@@ -5,11 +5,18 @@ import Post from './Post/Post';
 class MyPosts extends React.Component {
   render() {
 
-    const postData = [
-      {id: 1 , message: "ну вот как то так", like: 32 },
-      {id: 2, message: "довольно не плохо получилось", like: 2 }
+    const posts = [
+      { id: 1, message: "ну вот как то так", like: 32 },
+      { id: 2, message: "довольно не плохо получилось", like: 2 },
+      { id: 2, message: "три поросенка", like: 10 },
+      { id: 2, message: "во всем виноват Навальный", like: 175 }
     ]
 
+    let postsElement = posts.map((p) => {
+      return (
+        <Post message={p.message} like={p.like} />
+      )
+    })
     return (
       <div className={cl.content}>
         <h3>My posts</h3>
@@ -17,18 +24,14 @@ class MyPosts extends React.Component {
         <div>
           New post
           <div><textarea></textarea></div>
-          
+
           <div><button>Add post</button></div>
           <div><button>Remuve</button></div>
-          
-          
-            </div>
+
+
+        </div>
         <div className={cl.post}>
-          <Post message={postData[0].message} like={postData[0].like}/>
-          <Post message={postData[1].message} like={postData[1].like}/>
-          <Post />
-          <Post />
-          <Post />
+          {postsElement}
         </div>
       </div>
 
